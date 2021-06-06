ec0e5f65764e55c7cfe571f640fa968a3a80849f
syntonym <syntaar@gmail.com>
2021-06-06 21:18:00 +0200

    Fix app not stopping when window is closed

--- menqu/plot.py
+++ menqu/plot.py
@@ -123,11 +123,6 @@ class WellExcluder:
     def get_excluded_wells(self):
         return [parse_well(well.strip()) if well.strip() else None for well in self._tp.value.split(",")]
 
-
-
-
-
-
 class App:
 
     def __init__(self):
@@ -159,7 +154,7 @@ class App:
         button_export.on_click(lambda: asyncio.ensure_future(self.export_file_dialog()))
         _buttons.append(button_export)
 
-        button_exit = Button(label="Exit", width=BUTTON_WIDTH)
+        button_exit = Button(label="Exit", width=BUTTON_WIDTH, name="ExitButton")
         button_exit.on_click(lambda: asyncio.ensure_future(self.exit()))
         _buttons.append(button_exit)
 
@@ -434,77 +429,118 @@ class App:
         data = get_fake_data2()
         self.load_data(data)
 
-def start_py_web_view(port):
+def start_py_web_view(port, bokeh_server):
     window = webview.create_window('menqu', 'http://localhost:5006/')
 
-    webview.start(func=start_zmq_window_server, args=(window, port), debug=True)
+    web_view_thread = WebViewThread(window, port)
 
-def start_zmq_window_server(window, port):
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind(f"tcp://127.0.0.1:{port}")
+    window.closing += bokeh_server.stop
+    window.closing += web_view_thread.stop
 
-    while True:
-        msg = socket.recv()
-        if msg[:4] == b"SAVE":
-            suggested_name = msg[4:].decode("utf-8")
-            filename = window.create_file_dialog(webview.SAVE_DIALOG, directory=os.getcwd(), save_filename=suggested_name + ".menqu")
-            if filename:
-                filename = "".join(filename)
-            else:
-                filename = ""
-            socket.send(filename.encode("utf-8"))
-        elif msg == b"LOAD":
-            filename = window.create_file_dialog(webview.OPEN_DIALOG, directory=os.getcwd(), save_filename='test.menqu')
-            if filename:
-                filename = "".join(filename)
-            else:
-                filename = ""
-            socket.send(filename.encode("utf-8"))
-        elif msg == b"EXPORT":
-            filename = window.create_file_dialog(webview.SAVE_DIALOG, directory=os.getcwd(), save_filename='test.svg')
-            if filename:
-                filename = "".join(filename)
-            else:
-                filename = ""
-            socket.send(filename.encode("utf-8"))
-        elif msg == b"EXIT":
-            socket.send(b"BYE")
-            window.destroy()
-            sys.exit(0)
+    webview.start()
 
+class WebViewThread(threading.Thread):
 
-def start_server(port):
-    # Setting num_procs here means we can't touch the IOLoop before now, we must
-    # let Server handle that. If you need to explicitly handle IOLoops then you
-    # will need to use the lower level BaseServer class.
+    def __init__(self, window, port):
+        super().__init__()
+        self.window = window
+        self.port = port
+        self._stopping = False
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    app = App()
+    def stop(self):
+        self._stopping = True
 
-    context = zmq.asyncio.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect(f"tcp://127.0.0.1:{port}")
+    def run(self):
+        window = self.window
+        port = self.port
 
-    app.socket = socket
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.setsockopt(zmq.RCVTIMEO, 1000)
+        socket.bind(f"tcp://127.0.0.1:{port}")
 
-    server = Server({'/': app.transform}, num_procs=1)
-    server.start()
-    #server.io_loop.add_callback(server.show, "/")
-    server.io_loop.start()
+        while True:
+            if self._stopping:
+                break
+            try:
+                msg = socket.recv()
+            except zmq.error.Again:
+                continue
+            if msg[:4] == b"SAVE":
+                suggested_name = msg[4:].decode("utf-8")
+                filename = window.create_file_dialog(webview.SAVE_DIALOG, directory=os.getcwd(), save_filename=suggested_name + ".menqu")
+                if filename:
+                    filename = "".join(filename)
+                else:
+                    filename = ""
+                socket.send(filename.encode("utf-8"))
+            elif msg == b"LOAD":
+                filename = window.create_file_dialog(webview.OPEN_DIALOG, directory=os.getcwd(), save_filename='test.menqu')
+                if filename:
+                    filename = "".join(filename)
+                else:
+                    filename = ""
+                socket.send(filename.encode("utf-8"))
+            elif msg == b"EXPORT":
+                filename = window.create_file_dialog(webview.SAVE_DIALOG, directory=os.getcwd(), save_filename='test.svg')
+                if filename:
+                    filename = "".join(filename)
+                else:
+                    filename = ""
+                socket.send(filename.encode("utf-8"))
+            elif msg == b"EXIT":
+                socket.send(b"BYE")
+                window.destroy()
+                sys.exit(0)
+
+class BokehServer(threading.Thread):
+
+    def __init__(self, port):
+        self.port = port
+        super().__init__()
+
+    def run(self):
+        # Setting num_procs here means we can't touch the IOLoop before now, we must
+        # let Server handle that. If you need to explicitly handle IOLoops then you
+        # will need to use the lower level BaseServer class.
+
+        self.loop = loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        app = App()
+
+        self.context = context = zmq.asyncio.Context()
+        self.socket = socket = context.socket(zmq.REQ)
+        socket.setsockopt(zmq.LINGER, 1)
+        socket.connect(f"tcp://127.0.0.1:{self.port}")
+
+        app.socket = socket
+
+        self.server = server = Server({'/': app.transform}, num_procs=1)
+        server.start()
+        #server.io_loop.add_callback(server.show, "/")
+        server.io_loop.start()
+
+    def stop(self):
+        self.loop.call_soon_threadsafe(self._stop)
+
+    def _stop(self):
+
+        self.loop.stop()
+        self.context.destroy(linger=0)
+        self.context.term()
+        self.stop()
 
 def _main_pywebview():
     PORT = 21934
 
-    webview = threading.Thread(target=start_server, args=(PORT,), daemon=True)
+    webview = BokehServer(PORT)
     webview.start()
 
     #webview.join()
 
     time.sleep(0.3)
 
-    start_py_web_view(PORT)
+    start_py_web_view(PORT, webview)
 
 @click.command()
 @click.option("--update/--no-update", default=True)
